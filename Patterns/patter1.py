eefdd3ca9dc1444b118acffd938c98d18a125bf5
Lelouch0203 <jaiv2k7@gmail.com>
2025-03-14 22:02:57 +0530

    "Uploaded"

--- Patterns/patter1.py
+++ Patterns/patter1.py
@@ -63,3 +63,50 @@ for i in range(0,n):
     print()
     
 print()
+
+for i in range(0,n):    
+    for j in range(0,n-i-1):
+        print(" ",end="")
+    for j in range(0,i+1):
+        print("*",end="")
+    for j in range(0,i):
+        print("*",end="")
+    print()
+for i in range(0,n):
+    for j in range(0,i):
+        print("",end=' ')
+    for j in range(0,11-(2*(i+1))):
+        print("*",end='')
+    for j in range(0,i):
+        print("",end=' ')
+    print()
+    
+print()
+
+for i in range(0,2*n-1):
+    if i>4:
+        for j in range(0,2*n-(i+1)):
+            print("*",end=" ")
+    else:
+        for j in range(0,i+1):
+            print("*",end=" ")
+    
+    print()
+    
+print()
+
+for i in range(0,n):
+    for j in range(0,i+1):
+        if (i+j)%2==0:
+            print(1,end="")
+        else:
+            print(0,end="")
+    print()
+
+print()
+
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     for j in range(0,2*(n-i)):
+        
