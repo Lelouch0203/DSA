fab9bee4c0396a1c1281a5a8d6c636e1264ca9ca
Lelouch0203 <jaiv2k7@gmail.com>
2025-03-23 00:31:52 +0530

    "uploaded"

---
+++ LeetcodeSolutions/0008_StringToInteger.py
@@ -0,0 +1,25 @@
+def myAtoi(self, s: str) -> int:
+    s=s.strip()
+    if not s:
+        return 0
+        
+    ans= "-" if s[0]=="-" else ""
+
+    nums="1234567890"
+    start=0
+    if s[0]=="-" or s[0]=="+":
+        start+=1 
+    for i in range(start,len(s)):
+        if s[i] in nums:
+            ans+=s[i]
+        else:
+            break
+
+    result = int(ans) if ans not in ["", "-"] else 0
+    INT_MAX = 2**31 - 1
+    INT_MIN = -2**31
+    if result < INT_MIN:
+        return INT_MIN
+    if result > INT_MAX:
+        return INT_MAX
+    return result
