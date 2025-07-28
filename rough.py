4da7e855910d39c2585d2dc79da2fe3f337efaeb
Lelouch0203 <jaiv2k7@gmail.com>
2025-07-28 14:55:01 +0530

    Re - Structured

--- rough.py
+++ rough.py
@@ -1,16 +1,9 @@
-# Hashing immutable objects
-string_hash = hash("hello")
-int_hash = hash(123)
-float_hash = hash(3.14)
-tuple_hash = hash((1, 2, 3))
+import math
+arr = [1,2,3,4]
 
-print(f"Hash of 'hello': {string_hash}")
-print(f"Hash of 123: {int_hash}")
-print(f"Hash of 3.14: {float_hash}")
-print(f"Hash of (1, 2, 3): {tuple_hash}")
-
-# Attempting to hash mutable objects will raise a TypeError
-try:
-    list_hash = hash([1, 2, 3])
-except TypeError as e:
-    print(f"Error hashing list: {e}")
+prd = math.prod(arr)
+for i in range(len(arr)):
+    arr[i]=prd/arr[i]
+    # i=prd/i
+arr = list(map(int,arr))
+print(arr)
